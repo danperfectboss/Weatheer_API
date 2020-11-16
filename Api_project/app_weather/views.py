57620ca253a326605c467ba1d1e3a05ae502d413
boss <dapeme.boss@gmail.com>
2020-11-16 01:45:29 -0600

    Se realizo la validación para que los campos no fueran vacios en la petición, además se agregaron algunas estandarizaciones para los parametros entrantes: poner el código del país en minusculas, y poner la primer letra de la ciudad en mayúsculas

--- Api_project/app_weather/views.py
+++ Api_project/app_weather/views.py
@@ -3,22 +3,30 @@ from django.views.generic import TemplateView
 from app_weather.logic import Weather
 from rest_framework.views import APIView
 from rest_framework.response import Response
+'''Implementados para el uso de cache'''
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+
 
 # Create your views here.
-class WeatherView(APIView):
 
+class WeatherView(APIView):
+    @method_decorator(cache_page(60*4))
+    @method_decorator(vary_on_cookie)
     def get(self, request):
 
         City = str(request.GET.get('city')).capitalize()
         Country = str(request.GET.get('country')).lower()
 
-
-        obj = Weather().search(City,Country)
-
-        # return render(request,'index.html')
-        return Response({'datac':obj})
-    
-    def indexView(TemplateView):
-        template_name = "index.html"
+        if City and Country:
+            obj = Weather().search(City,Country)
+        else: raise ValueError('El campo de Ciudad y codigo de país deben de llevar datos')
         
+        return Response({'Response':obj})
+    
+    
+
+def indexView(request):
+        return render(request,'index.html')  
 
