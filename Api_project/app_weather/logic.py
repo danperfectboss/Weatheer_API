787a549763572db7ce7e1f11e7bf94804af22bd4
boss <dapeme.boss@gmail.com>
2020-11-16 01:42:38 -0600

    Se crearon algunos metodos para la transformación del timestamp de UNIX a UTC se reailzaron validaciones para el método de buscar, se creo la función para construir la respuesta, se creo la función que transforma los grados kelvin a grados centigrados

--- Api_project/app_weather/logic.py
+++ Api_project/app_weather/logic.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 import json
 import requests
+from datetime import datetime,timedelta
 
 class Weather():
     def __init__(self):
@@ -11,6 +12,42 @@ class Weather():
         url = "https://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=%s"%(City,Country,self.api_key)
         response = requests.get(url)
         data = json.loads(response.text)
-        print(data)
+        if data['cod'] == 200:
+            data = Weather().build_the_response(data)
+        else: raise ValueError('Ingresa ciudad valida')
         
         return data
+
+
+        
+
+    def build_the_response(self, data):
+        wt = Weather()
+        response = {
+            'location_name':data['name'] +' , '+ str(data['sys']['country']).lower(),
+            'temperature':str(wt.kelvin_to_celcius(data['main']['temp']))+' °C',
+            'wind': str(data['wind']['speed']) + ' m/s',
+            'cloudiness':str(data['weather'][0]['description']).capitalize(),
+            'pressure': str(data['main']['pressure'])+ ' hpa',
+            'humidity':str(data['main']['humidity']) + '%',
+            'sunrise': wt.transform_unix_to_cst(data['sys']['sunrise']) + ' UTC',
+            'sunset': wt.transform_unix_to_cst(data['sys']['sunset']) + ' UTC',
+            'geo_coordinates': '['+ str(data['coord']['lat'])+', ' + str(data['coord']['lon']) +']',
+            'requested_time':datetime.now().strftime('%H:%M:%S')
+        }
+        return response
+
+    def transform_unix_to_cst(self, hour):
+        hour = int(hour)
+        if(hour != 0):
+            hour = datetime.utcfromtimestamp(hour).strftime('%H:%M:%S')
+            # hour = timezone('US/Central').localize(hour)
+
+
+        return hour 
+
+    def kelvin_to_celcius(self,temp):
+        temp = float(temp)
+        celcius = temp - 273.15
+
+        return round(celcius,2)
