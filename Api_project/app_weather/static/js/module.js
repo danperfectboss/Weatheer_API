d555cc204f39da3b69a0c87444a78f0ed709cbee
boss <dapeme.boss@gmail.com>
2020-11-17 12:33:12 -0600

    Se crearon cambios y validaciones en la función de validateEmpty

--- Api_project/app_weather/static/js/module.js
+++ Api_project/app_weather/static/js/module.js
@@ -15,11 +15,14 @@ function validateEmpty(){
     var RBjson = $('#Json').is(':checked')
     var RBpretty = $('#Pretty').is(':checked')
 
-    if ((city != '' && country != '') && (RBjson === True || RBpretty === True)){
+    if ((city != '' && country != '')){
         
-        checkRadioButton()
-        
-        return true
+        if(checkRadioButton()){
+            return true    
+        }else{
+            alertify.error('Radio buttons must not be empty');
+            return false
+        }
         
         
     }else{
@@ -32,8 +35,14 @@ function checkRadioButton(){
 
     if($('#Json').is(':checked')){
         $('#formID').attr('action', '/weather');
+
     }else if($('#Pretty').is(':checked')){
         $('#formID').attr('action', '/pretty_weather');
     }
+    
+    if($('#Json').is(':checked') || $('#Pretty').is(':checked')){
+
+        return true
+    }else return false
 
 }
